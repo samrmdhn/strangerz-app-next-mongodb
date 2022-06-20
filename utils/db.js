743dd71dccb18f61898231dcd5b96797abf62847
samrmdhn <thesyams.rebel8@gmail.com>
2022-06-20 18:09:12 +0700

    db change

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,6 @@
-import mongoose from "mongoose";const db = async () =>
-  mongoose.connect("mongodb://localhost:27017/db_strangerz");
-
+import mongoose from "mongoose";
+const db = async () =>
+  mongoose.connect(
+    "mongodb+srv://samrmdhn:yHV7hmVE666@cluster0.laf5pqx.mongodb.net/test"
+  );
 export default db;
